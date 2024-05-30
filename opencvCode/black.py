888825effae946e956dcc87ab3789d3536322334
Jeromel Pushparaj <jeromal03@gmail.com>
2024-05-30 10:55:37 +0530

    feat: emotion detection and mobile connection using ip

--- opencvCode/black.py
+++ opencvCode/black.py
@@ -1,8 +1,11 @@
 import cv2
-img = cv2.imread("ex1.jpg")
+img = cv2.imread("imgs//tigger.jpg")
 grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-thresholdImg = cv2.threshold(grayImg,160,255,cv2.THRESH_BINARY)[1]
+thresholdImg = cv2.threshold(grayImg,130,225,cv2.THRESH_BINARY)[1]
 
 cv2.imshow("origi", img)
 cv2.imshow("Threshold.jpg",thresholdImg)
+
+cv2.waitKey(10000)
+cv2.destroyAllWindows()
--- opencvCode/blur.py
+++ opencvCode/blur.py
@@ -1,7 +1,9 @@
 import cv2
-img=cv2.imread('ex1.jpg')
+img=cv2.imread('D:\\playground\\Tessolve trainning\\imgs\\car image.jpg')
 gm=cv2.GaussianBlur(img,(41,41),0)
-gm1=cv2.GaussianBlur(img,(21,21),0)
+gm1=cv2.GaussianBlur(img,(11,11),0)
+
 cv2.imshow("org",img)
 cv2.imshow("gm",gm)
 cv2.imshow("gm1",gm)
+cv2.waitKey(10000)
